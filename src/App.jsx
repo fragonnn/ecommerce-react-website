b3382bc2668ee88c19c7df782534f26e5baffd17
achraf <achrafsamih2006@hotmail.fr>
2026-04-07 16:58:37 +0100

    Added auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import Home from "./Pages/Home";
 import Auth from "./Pages/Auth";
 import Checkout from "./Pages/Checkout";
 import Navbar from "./Components/Navbar";
+import AuthProvider from "./context/AuthContext";
 import "./App.css";
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   );
 }
 
